a0d15cc6e33f4ef35e13eaa36e6aecbfb4cc8d0f
pabois <pierreandre.boissinot@noesya.coop>
2025-07-03 15:42:06 +0200

    upgrade ruby 3.4.4 and rails 8

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_06_18_152740) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_18_152740) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
   enable_extension "unaccent"
 
   create_table "active_storage_attachments", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
