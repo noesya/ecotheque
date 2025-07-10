a9b335f6ebccdce3f5bc70ada5e7cb8bfceda371
Arnaud Levy <contact@arnaudlevy.com>
2025-07-10 12:52:57 +0200

    better

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,12 +4,12 @@ class UserMailer < ApplicationMailer
   def welcome_message
     @user = params[:user]
     return unless should_send?(@user.email)
-    welcome_message_content = MailerContent.find_by(identifier: 'welcome_message')
-    return unless welcome_message_content.try(:published?)
-    @body = welcome_message_content.body
+    content = MailerContent.find_by(identifier: 'welcome_message')
+    return unless content.try(:published?)
+    @body = content.body
     mail(
       to: @user.email,
-      subject: welcome_message_content.subject
+      subject: content.subject
     )
   end
 
