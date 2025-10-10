7faffefa2f3abe776a1ebfcc02890cbc9c0388e1
Sébastien Gaya <sebastien.gaya@gmail.com>
2025-10-10 16:10:17 +0200

    no asset compile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,12 +17,14 @@ Rails.application.configure do
 
   # Cache assets for far-future expiry since they are all digest stamped.
   config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
-  
+
   # Compress CSS using a preprocessor.
   config.assets.js_compressor = :terser
-  
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
+
+  config.assets.compile = false
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :scaleway
